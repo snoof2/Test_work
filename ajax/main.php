800e2824289f36f7c2580f8d46e2fb2a5b83eba0
Aleksander <55217294+snoof2@users.noreply.github.com>
2020-07-13 02:54:28 +0300

    Update main.php

--- ajax/main.php
+++ ajax/main.php
@@ -9,8 +9,8 @@ $password_form = $_POST['password'];
     $surname = array("123","123","123");
     $password = array("123","123","123");
 
-
 $success = null;
+
 foreach ($email as $z) {
 	if (in_array($email_form, $email)) {
 		$success = false;
@@ -18,17 +18,17 @@ foreach ($email as $z) {
 	}else {
 		$success = true; 
 		echo $success;
+		
 	}
 }
-ob_start();
- 
-
-foreach (getallheaders() as $name => $value) {
-	echo "$name: $value\n";
+if ($success === false) {
+    $log = date('Y-m-d H:i:s') . ' '. 'Пользователь с данным email уже существует =>'. ' ' . $email_form;
+    file_put_contents(__DIR__ . '/log.txt', $log . PHP_EOL, FILE_APPEND);
+}else {
+    $log = date('Y-m-d H:i:s'). ' ' . 'Пользователь успешно зарегистрирован =>'. ' ' . $email_form;
+	file_put_contents(__DIR__ . '/log.txt', $log . PHP_EOL, FILE_APPEND);
 }
- 
-$log = date('Y-m-d H:i:s') . PHP_EOL . ob_get_clean() . PHP_EOL;
-file_put_contents(__DIR__ . '/log.txt', $log, FILE_APPEND);
+
 
 
 
